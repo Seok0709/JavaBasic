fe4166be72a7830371871ab6dac26c2312906da9
Seok0709 <sbs4597@naver.com>
2021-02-26 17:19:44 +0900

    21-02-26 day5 완성

--- day5/Group.java
+++ day5/Group.java
@@ -1,5 +1,27 @@
 package day5;
 
-public class Group {
+import java.util.Scanner;
 
+public class Group {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Student[] arr = new Student[3]; //학생 3명 있는반
+		for(int i =0; i<arr.length; i++) {
+			System.out.println("[이름]"+arr[i].getName()+"[점수] "+arr[i].getScore()+"\n");
+			System.out.println("이름:    ");
+			String name = sc.next();
+			System.out.println("점수:    ");
+			int score = sc.nextInt();
+			Student st1 = new Student();
+			st1.setName(name);
+			st1.setScore(score);
+			arr[i] = st1;
+			
+		}
+		//첫번쨰 학생
+		//두번째 학생
+		for(int i=0; i<arr.length;i++) {
+			System.out.println("[이름]"+arr[i].getName()+"[점수] "+arr[i].getScore()+"\n");
+		}
+	}
 }
